ab648bd42c2712b00898fe7eea8dbcc612a35fb2
Oskari Liukku <oliukku@gmail.com>
2019-01-25 22:14:17 +0200

    rename playermove to basicmove

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour, IMover
+public class BasicMovement : MonoBehaviour, IMover
 {
     private Rigidbody2D _rb;
 
