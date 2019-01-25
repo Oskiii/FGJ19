b61dd798a30bc4512449a4d70a8e833224c54cb8
Oskari Liukku <oliukku@gmail.com>
2019-01-25 22:26:13 +0200

    Rotate to direction

---
+++ Assets/RotateSpriteToVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSpriteToVelocity : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField]
+    private Transform _sprite;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        _sprite.up = _rb.velocity;
+    }
+}
