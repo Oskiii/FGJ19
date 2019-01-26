836bc52efd2c3c3f328ef7274cdc6b9715ab590b
Oskari Liukku <oliukku@gmail.com>
2019-01-26 12:16:10 +0200

    Enemy spawn waves

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _spawnPoints;
+
+    [SerializeField]
+    private List<EnemyWave> _waves;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        foreach (var wave in _waves)
+        {
+            var spawnPos = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
+            yield return wave.Spawn(spawnPos);
+        }
+    }
+}
---
+++ Assets/Scripts/Waves/EnemyWave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyWave", menuName = "EnemyWave", order = 0)]
+public class EnemyWave : ScriptableObject
+{
+    [SerializeField]
+    private float DelayBefore;
+    [SerializeField]
+    private List<EnemySpawnAmounts> EnemyAmounts;
+    [SerializeField]
+    private float DelayAfter;
+
+    [System.Serializable]
+    private class EnemySpawnAmounts
+    {
+        public GameObject Enemy;
+        public int Amount;
+    }
+
+    public IEnumerator Spawn(Transform spawnPoint)
+    {
+        yield return new WaitForSeconds(DelayBefore);
+
+        foreach (var e in EnemyAmounts)
+        {
+            for (int i = 0; i < e.Amount; i++)
+            {
+                var enemy = Instantiate(e.Enemy, spawnPoint.position, Quaternion.identity);
+            }
+        }
+
+        yield return new WaitForSeconds(DelayAfter);
+    }
+}
