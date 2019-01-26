e9954cf2832f2fd0b9ca9feb8a327925abbf08a7
Oskari Liukku <oliukku@gmail.com>
2019-01-26 11:52:34 +0200

    Bullets deal damage

---
+++ Assets/Scripts/BasicBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBullet : MonoBehaviour
+{
+    [SerializeField]
+    private int _damage;
+
+    private float _lifeTime = 3f;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        Destroy(gameObject, _lifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var health = other.gameObject.GetComponent<Health>();
+        if (health == null) return;
+
+        health.Damage(_damage);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int _startingHealth = 30;
+    public int Hp { get; private set; }
+
+    private void Start()
+    {
+        Hp = _startingHealth;
+    }
+
+    public void Damage(int dmg)
+    {
+        Hp -= dmg;
+        print("Take damage " + Hp);
+    }
+}
