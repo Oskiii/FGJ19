4b51c59876322319b8ef335bbc91ae6cd91b26d5
Oskari Liukku <oliukku@gmail.com>
2019-01-26 17:33:26 +0200

    Organize folders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-  [SerializeField]
-  private Transform _gunHolder;
+    [SerializeField]
+    private Transform _gunHolder;
 
-  [HideInInspector]
-  public Gun CurrentGun;
+    [HideInInspector]
+    public Gun CurrentGun;
 
-  void Start()
-  {
-    CurrentGun = GetComponentInChildren<Gun>();
-  }
+    void Start()
+    {
+        CurrentGun = GetComponentInChildren<Gun>();
+        GetComponent<Health>().OnDie += LoseGame;
+    }
 
-  public void SetGun(Gun gun)
-  {
-    Destroy(GetComponentInChildren<Gun>().gameObject);
-    GameObject newGun = Instantiate(gun.gameObject, _gunHolder);
-    newGun.transform.position = _gunHolder.transform.position;
-    CurrentGun = newGun.GetComponent<Gun>();
-  }
+    public void SetGun(Gun gun)
+    {
+        Destroy(GetComponentInChildren<Gun>().gameObject);
+        GameObject newGun = Instantiate(gun.gameObject, _gunHolder);
+        newGun.transform.position = _gunHolder.transform.position;
+        CurrentGun = newGun.GetComponent<Gun>();
+    }
+
+    private void LoseGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
 }
