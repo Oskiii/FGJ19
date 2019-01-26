b34a4812c98b380f20e270a1726b65867a257d07
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-26 12:45:02 +0200

    added tag to player prefab, basic enemies shoot

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
   private IMover _movement;
+  private IGun _gun;
   private Vector2 _targetPos;
 
   void Start () {
-    _movement = GetComponent<IMover> ();
+    _movement = GetComponent<IMover>();
+    _gun = GetComponentInChildren<IGun>();
   }
 
   void Update () {
-    _targetPos = GameObject.FindGameObjectWithTag ("Player").transform.position;
+    _targetPos = GameObject.FindGameObjectWithTag("Player").transform.position;
     var moveDir = new Vector2 (_targetPos.x - transform.position.x, _targetPos.y - transform.position.y).normalized;
     _movement.SetDirection (moveDir);
+
+    var shouldShoot = true;
+    _gun.SetShouldShoot(shouldShoot);
   }
 }
