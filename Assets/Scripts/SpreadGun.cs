59917cae78944141f92e35d0b3ac48f865e4d470
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-26 13:49:20 +0200

    created script folders, spread gun script

---
+++ Assets/Scripts/SpreadGun.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadGun : MonoBehaviour, IGun {
+  [SerializeField]
+  private Rigidbody2D _bullet;
+  [SerializeField]
+  private Transform _muzzlePoint;
+  [SerializeField]
+  private float _shootForce = 1f;
+  [SerializeField]
+  private int _bulletAmount = 3;
+  [SerializeField]
+  private float _shootCooldown = 0.1f;
+  private float _lastShotTime = 0f;
+  private float _angleBetweenBullets;
+  private bool _shouldShoot = false;
+
+  public void SetShouldShoot (bool shouldShoot) {
+    _shouldShoot = shouldShoot;
+    _angleBetweenBullets = 60 / _bulletAmount;
+  }
+
+  private void Update () {
+    if (_shouldShoot && Time.time >= _lastShotTime + _shootCooldown) {
+      Shoot ();
+    }
+  }
+
+  public void Shoot () {
+    _lastShotTime = Time.time;
+
+    for (int i = 0; i < _bulletAmount; i++) {
+      GameObject bullet = Instantiate (_bullet.gameObject, _muzzlePoint.position, Quaternion.identity);
+      var bulletRb = bullet.GetComponent<Rigidbody2D> ();
+      Vector2 bulletDir = Rotate (transform.up, _angleBetweenBullets * i - _angleBetweenBullets);
+
+      bulletRb.AddForce (bulletDir * _shootForce, ForceMode2D.Impulse);
+      Destroy (bullet, 3f);
+    }
+  }
+
+  public static Vector2 Rotate (Vector2 v, float degrees) {
+    float sin = Mathf.Sin (degrees * Mathf.Deg2Rad);
+    float cos = Mathf.Cos (degrees * Mathf.Deg2Rad);
+
+    float tx = v.x;
+    float ty = v.y;
+    v.x = (cos * tx) - (sin * ty);
+    v.y = (sin * tx) + (cos * ty);
+    return v;
+  }
+}
