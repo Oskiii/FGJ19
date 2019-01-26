464cc847b8c3fcd4aa21fe94b83cac814b5eb799
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-26 12:15:44 +0200

    enemyai script

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+  private IMover _movement;
+  private Vector2 _targetPos;
+
+  void Start () {
+    _movement = GetComponent<IMover> ();
+  }
+
+  void Update () {
+    _targetPos = GameObject.FindGameObjectWithTag ("Player").transform.position;
+    var moveDir = new Vector2 (_targetPos.x - transform.position.x, _targetPos.y - transform.position.y).normalized;
+    _movement.Move (moveDir);
+  }
+}
--- Assets/Scripts/JumpyMovement.cs
+++ Assets/Scripts/JumpyMovement.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpyMovement : MonoBehaviour, IMover
-{
+public class JumpyMovement : MonoBehaviour, IMover {
     private Rigidbody2D _rb;
     private float _timeSinceLastJump = 0f;
     private Vector2 _dir;
@@ -11,44 +10,38 @@ public class JumpyMovement : MonoBehaviour, IMover
 
     [SerializeField]
     private float _moveSpeed = 10f;
+    [SerializeField]
+    private float _jumpTimer = 2f;
 
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
+    private void Start () {
+      _rb = GetComponent<Rigidbody2D> ();
+      _jumping = false;
+    }
+
+    private void Update () {
+      if (_timeSinceLastJump >= _jumpTimer) {
+        _jumping = true;
+        _timeSinceLastJump = 0.0f;
+      }
+
+      if (_timeSinceLastJump < _jumpTimer && _timeSinceLastJump > 1f && _jumping) {
         _jumping = false;
+        _rb.velocity = Vector2.zero;
+      }
+
+      if (_jumping) {
+        Jump (_dir);
+      }
+
+      _timeSinceLastJump += Time.deltaTime;
     }
 
-    private void Update()
-    {
-        if (_timeSinceLastJump >= 2.0f)
-        {
-            _jumping = true;
-            _timeSinceLastJump = 0.0f;
-        }
-
-        if (_timeSinceLastJump < 2.0f && _timeSinceLastJump > 1.0f)
-        {
-            _jumping = false;
-        }
-
-        if (_jumping)
-        {
-            Jump(_dir);
-        }
-
-        _timeSinceLastJump += Time.deltaTime;
+    public void SetDirection (Vector2 dir) {
+      if (!_jumping) {
+        _dir = dir;
+      }
     }
 
-    public void SetDirection(Vector2 dir)
-    {
-        if (!_jumping)
-        {
-            _dir = dir;
-        }
+    private void Jump (Vector2 dir) {
+      _rb.velocity = (dir * _moveSpeed);
     }
-
-    private void Jump(Vector2 dir)
-    {
-        _rb.velocity = (dir * _moveSpeed);
-    }
-}
