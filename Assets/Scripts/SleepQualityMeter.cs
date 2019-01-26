22f196aaeb1f576d44eae723251d29842958f0a2
Oskari Liukku <oliukku@gmail.com>
2019-01-26 14:09:54 +0200

    Sleep quality meter

---
+++ Assets/Scripts/SleepQualityMeter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SleepQualityMeter : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxQuality = 10f;
+    private Slider _slider;
+    private float _quality;
+
+    private void Start()
+    {
+        _slider.maxValue = _maxQuality;
+    }
+
+    public void ReduceQuality(float amount)
+    {
+        _quality -= amount;
+        _slider.value = _quality;
+    }
+}
