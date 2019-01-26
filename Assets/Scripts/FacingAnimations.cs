8e00051993446cce692796cff47cfb10d77e7fe7
Oskari Liukku <oliukku@gmail.com>
2019-01-26 16:48:59 +0200

    Player sprite faces mouse

---
+++ Assets/Scripts/FacingAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingAnimations : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 screenPos = new Vector3();
+
+        screenPos.x = Input.mousePosition.x;
+        screenPos.y = Input.mousePosition.y;
+        screenPos.z = -Camera.main.transform.position.z;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        Vector3 vecToMouse = worldPos - transform.position;
+
+        _animator.SetFloat("VelX", vecToMouse.x);
+        _animator.SetFloat("VelY", vecToMouse.y);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
   [SerializeField]
-  private Transform _sprite;
+  private Transform _gunHolder;
 
+  [HideInInspector]
   public Gun CurrentGun;
 
-  void Start () {
-    CurrentGun = GetComponentInChildren<Gun> ();
+  void Start()
+  {
+    CurrentGun = GetComponentInChildren<Gun>();
   }
 
-  public void SetGun (Gun gun) {
-    Destroy (GetComponentInChildren<Gun> ().gameObject);
-    GameObject newGun = Instantiate (gun.gameObject, _sprite);
-    newGun.transform.position = _sprite.transform.position;
-    CurrentGun = newGun.GetComponent<Gun> ();
+  public void SetGun(Gun gun)
+  {
+    Destroy(GetComponentInChildren<Gun>().gameObject);
+    GameObject newGun = Instantiate(gun.gameObject, _gunHolder);
+    newGun.transform.position = _gunHolder.transform.position;
+    CurrentGun = newGun.GetComponent<Gun>();
   }
 }
