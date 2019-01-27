29a61c80378f3791c026aeb2bff108a8544a12be
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-27 15:34:44 +0200

    fixed issues

--- Assets/IntroManager.cs
+++ Assets/IntroManager.cs
@@ -2,49 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroManager : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject _hud;
-    [SerializeField]
-    private GameObject _letterPrefab;
-    [SerializeField]
-    private List<GameObject> _spawners;
+public class IntroManager : MonoBehaviour {
+  [SerializeField]
+  private GameObject _hud;
+  [SerializeField]
+  private GameObject _letterPrefab;
+  [SerializeField]
+  private List<GameObject> _spawners;
+  [SerializeField]
+  private EnemySpawner _enemySpawner;
 
-    private void Awake()
-    {
-        foreach (var item in _spawners)
-        {
-            item.SetActive(false);
-        }
+  private void Awake () {
+    foreach (var item in _spawners) {
+      item.SetActive (false);
     }
 
-    private void Start()
-    {
-        if (DifficultyManager.Instance.Day != 0)
-        {
-            foreach (var item in _spawners)
-            {
-                item.SetActive(true);
-            }
-        }
-        else
-        {
-            _hud.SetActive(false);
+    _enemySpawner.ShouldSpawn = false;
+  }
 
-            var letterObj = Instantiate(_letterPrefab);
-            var letter = letterObj.GetComponent<IntroLetter>();
-            letter.OnClosed += StartPlay;
-        }
+  private void Start () {
+    if (DifficultyManager.Instance.Day != 0) {
+      foreach (var item in _spawners) {
+        item.SetActive (true);
+      }
+      _enemySpawner.ShouldSpawn = true;
+    } else {
+      _hud.SetActive (false);
+
+      var letterObj = Instantiate (_letterPrefab);
+      var letter = letterObj.GetComponent<IntroLetter> ();
+      letter.OnClosed += StartPlay;
+    }
+  }
+
+  private void StartPlay () {
+    _hud.SetActive (true);
+
+    foreach (var item in _spawners) {
+      item.SetActive (true);
     }
 
-    private void StartPlay()
-    {
-        _hud.SetActive(true);
-
-        foreach (var item in _spawners)
-        {
-            item.SetActive(true);
-        }
-    }
+    _enemySpawner.ShouldSpawn = true;
+  }
 }
