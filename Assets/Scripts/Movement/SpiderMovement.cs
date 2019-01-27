63a3afc1d0db7cad05a71ec0a3030d45cb6bec08
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-27 10:59:40 +0200

    spider enemy

---
+++ Assets/Scripts/Movement/SpiderMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour, IMover {
+  // Start is called before the first frame update
+  [SerializeField]
+  private float _descentHeight = 7f;
+  void Start () {
+    transform.DOMoveY (transform.position.y - _descentHeight, 2);
+  }
+
+  public void SetDirection (Vector2 dir) {
+    return;
+  }
+}
