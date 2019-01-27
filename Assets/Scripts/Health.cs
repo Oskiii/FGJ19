98b385c88dc726cc8d98b3086bb0121a28a86b02
Miika Kanerva <miika.kanerva@aalto.fi>
2019-01-27 14:36:32 +0200

    limited healing to max health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,56 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
-{
-    [SerializeField]
-    private int _startingHealth = 30;
-    [SerializeField]
-    private HealthBar _healthBar;
+public class Health : MonoBehaviour {
+  [SerializeField]
+  private int _startingHealth = 30;
+  [SerializeField]
+  private HealthBar _healthBar;
 
-    public int Hp { get; private set; }
-    private bool _dead = false;
+  public int Hp { get; private set; }
+  private bool _dead = false;
 
-    public event Action OnDamage;
+  public event Action OnDamage;
 
-    public event Action OnDie;
+  public event Action OnDie;
 
-    private void Start()
-    {
-        Hp = _startingHealth;
-        UpdateHealthbar();
+  private void Start () {
+    Hp = _startingHealth;
+    UpdateHealthbar ();
+  }
+
+  public void Damage (int dmg) {
+    Hp -= dmg;
+
+    OnDamage?.Invoke ();
+
+    if (Hp < 0) {
+      Hp = 0;
     }
 
-    public void Damage(int dmg)
-    {
-        Hp -= dmg;
+    UpdateHealthbar ();
 
-        OnDamage?.Invoke();
-
-        if (Hp < 0)
-        {
-            Hp = 0;
-        }
-
-        UpdateHealthbar();
-
-        if (!_dead && Hp <= 0)
-        {
-            _dead = true;
-            OnDie?.Invoke();
-        }
+    if (!_dead && Hp <= 0) {
+      _dead = true;
+      OnDie?.Invoke ();
     }
+  }
 
-    public void Heal(int amount)
-    {
-        Hp += amount;
-        UpdateHealthbar();
-    }
+  public void Heal (int amount) {
+    Hp += amount;
+    if (Hp > _startingHealth) Hp = _startingHealth;
+    UpdateHealthbar ();
+  }
 
-    private void UpdateHealthbar()
-    {
-        if (_healthBar == null) return;
+  private void UpdateHealthbar () {
+    if (_healthBar == null) return;
 
-        _healthBar.SetValue((float) Hp / (float) _startingHealth);
-    }
+    _healthBar.SetValue ((float) Hp / (float) _startingHealth);
+  }
 }
